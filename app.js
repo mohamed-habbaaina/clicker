ee5d7cd1fd372b6a206119615dee8e1508ee7303
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-03-07 12:34:29 +0100

    JS, Stored levels, localStorage

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // ****************** Selector *********************/
 // *************************************************/
 
+let click = localStorage.getItem("click") || 0;
+
+
 const scors = document.querySelector('.scors p');
 const clickerBtn = document.querySelector('.logo button');
 const levels = document.querySelectorAll('.btnLevel');
@@ -8,9 +11,13 @@ const levels = document.querySelectorAll('.btnLevel');
 // ****************** variables ********************/
 // *************************************************/
 
-let click = 0;
+// Stored levels (bonus).
+let setLcale_1 = JSON.parse(localStorage.getItem("setLcale_1")) || false;
+let setLcale_2 = JSON.parse(localStorage.getItem("setLcale_2")) || false;;
+let setLcale_3 = JSON.parse(localStorage.getItem("setLcale_3")) || false;;
+let setLcale_4 = JSON.parse(localStorage.getItem("setLcale_4")) || false;;
 
-// setIntervals
+
 let set_1;
 let set_2;
 let set_3;
@@ -25,21 +32,58 @@ let disabledLevel4 = true;
 // ****************** Listener *********************/
 // *************************************************/
 
+// buttons
 clickerBtn.addEventListener('click', clic);
 levels[0].addEventListener('click', level_1);
 levels[1].addEventListener('click', level_2);
 levels[2].addEventListener('click', level_3);
 levels[3].addEventListener('click', level_4);
 
+// load bonus
+window.addEventListener('load', loadGame);
 
 
+function loadGame() {
+
+    if(setLcale_1 === true){
+        set_1 = setInterval(clic, 1000);
+        disabledLevel1 = false;
+    }
+
+    if(setLcale_2 === true){
+        set_2 = setInterval(clic, 500);
+        disabledLevel2 = false;
+    }
+
+    if(setLcale_3 === true){
+        set_3 = setInterval(clic, 200);
+        disabledLevel3 = false;
+    }
+
+    if(setLcale_4 === true){
+        set_4 = setInterval(clic, 50);
+        disabledLevel4 = false;
+    }
+
+    
+}
 
 function clic(){
 
     click++;
-    scors.innerHTML = 'Scors:<br>' + click + ' pt.';
+    
+    localStorage.setItem("click", click);
 
-    if (click >= 10 && disabledLevel1 === true) {
+    scors.innerHTML = click + ' pt.';
+
+
+    // todo: level dynamic
+    // levelValue = levels[0].textContent;
+    // let clickValue = click + ' pt.';
+    // console.log(levelValue, clickValue);
+    
+
+    if (click == 10 && disabledLevel1 === true) {
 
         // display button level 1
         levels[0].disabled = false;
@@ -67,9 +111,15 @@ function clic(){
 
 function level_1(){
 
-    if(click >= 10){
+
+
+
+    if(click >= 10 || setLcale_1 === true){
         click -= 10;
         set_1 = setInterval(clic, 1000)
+
+        localStorage.setItem("setLcale_1", JSON.stringify(true));
+
 
         // display button == disabled.
         levels[0].disabled = true;
@@ -88,6 +138,9 @@ function level_2(){
         click -= 30;
         set_2 = setInterval(clic, 500)
 
+        localStorage.setItem("setLcale_2", JSON.stringify(true));
+
+
         // display button == disabled.
         levels[1].disabled = true;
         disabledLevel2 = false;
@@ -102,9 +155,11 @@ function level_2(){
 function level_3(){
 
 
-    if(click >= 100){
-        click -= 100;
+    if(click >= 50){
+        click -= 50;
         set_3 = setInterval(clic, 200)
+
+        localStorage.setItem("setLcale_3", JSON.stringify(true));
 
         // display button == disabled.
         levels[2].disabled = true;
@@ -119,9 +174,11 @@ function level_3(){
 function level_4(){
 
 
-    if(click >= 200){
-        click -= 200;
+    if(click >= 100){
+        click -= 100;
         set_4 = setInterval(clic, 50)
+
+        localStorage.setItem("setLcale_4", JSON.stringify(true));
 
         // display button == disabled.
         levels[3].disabled = true;
